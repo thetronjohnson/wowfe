81c6eecb1de0e9e3330516fd2a607352f185fe14
Kiran Johns <kiranjohns369@gmail.com>
2020-05-31 01:55:17 +0530

    dashboard modification

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,74 +1,109 @@
 <template>
-    <section>
-        <h1 class="title" id="profile"> {{ name }}'s Contributions</h1>
+  <section>
+    <div class="columns">
+      <div class="column is-half">
+        <h1 class="title" id="profile">{{ name }}'s Contributions</h1>
         <div class="container table">
-                <b-table
-                    :data="data"
-                    :columns="columns">
-                </b-table>
-        <b-field>
-        <router-link to="/add" class="button is-primary">
-                    Add
-        </router-link>
-        </b-field>
+          <b-table :data="data" :columns="columns"></b-table>
+          <b-field>
+            <router-link to="/add-data" class="button is-primary is-fullwidth">Add</router-link>
+          </b-field>
         </div>
-    </section>
+      </div>
+      <div class="column tiles">
+        <div class="card" v-for="gauge in gauges" :key="gauge">
+          <header class="card-header">
+            <p class="card-header-title">{{gauge.name}}</p>
+            <a href="#" class="card-header-icon" aria-label="more options">
+            </a>
+          </header>
+          <div class="card-content">
+            <div class="content">
+                <p>{{gauge.place}}</p>
+                <p>{{gauge.coordinates}}</p>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <a href="#" class="card-footer-item">Update</a>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-        name: 'Dashboard',
-        data() {
-            const data = [
-                { 'id': 1, 'Rain': '12', 'date': '2016-10-15 13:43:27' },
-                { 'id': 2, 'Rain': '15',  'date': '2016-12-15 06:00:53'},
-                { 'id': 3, 'Rain': '100',  'date': '2016-04-26 06:26:28'},
-                { 'id': 4, 'Rain': '2',  'date': '2016-04-10 10:28:46'},
-                { 'id': 5, 'Rain': '4',  'date': '2016-12-06 14:38:38'}
-            ]
+  name: "Dashboard",
+  data() {
+    const data = [
+      { id: 1, Rain: "12", date: "2016-10-15 13:43:27" },
+      { id: 2, Rain: "15", date: "2016-12-15 06:00:53" },
+      { id: 3, Rain: "100", date: "2016-04-26 06:26:28" },
+      { id: 4, Rain: "2", date: "2016-04-10 10:28:46" },
+      { id: 5, Rain: "4", date: "2016-12-06 14:38:38" }
+    ];
+    const gauges= [
+        {name:'Driftx',place:'tokyo',coordinates:'(75,65)'},
+        {name:'Driftx2',place:'muvattupuzha',coordinates:'(45,65)'},
+    ];
 
-            return {
-                data,
-                name: "Joe" ,
-                selected: data[1],
-                columns: [
-                    {
-                        field: 'id',
-                        label: 'ID',
-                        width: '40',
-                        numeric: true
-                    },
-                    {
-                        field: 'Rain',
-                        label: 'Rain in mm',
-                        centered: true
-                    },
-                    {
-                        field: 'date',
-                        label: 'Date',
-                        centered: true
-                    },
-                ]
-            }
+    return {
+      data,
+      gauges,
+      name: "Joe",
+      columns: [
+        {
+          field: "id",
+          label: "ID",
+          width: "40",
+          numeric: true
+        },
+        {
+          field: "Rain",
+          label: "Rain in mm",
+          centered: true
+        },
+        {
+          field: "date",
+          label: "Date",
+          centered: true
         }
-    }
+      ]
+    };
+  }
+};
 </script>
 <style scoped>
-    .table{
-        margin-left: 25vw;
-        margin-right: 25vw;
-    }
-     .title{
-     margin-top:10vw;
-     background-color:hsl(217, 71%, 53%);
-     color: white;
-     margin-left: 25vw;
-     margin-right: 25vw;
-     padding-top: 10px;
-     padding-bottom: 10px;
- }
- .button{
+.table {
+  margin-left: 5vw;
+  margin-right: 5vw;
+}
+.title {
+  margin-top: 3vw;
+  background-color: hsl(217, 71%, 53%);
+  color: white;
+  margin-left: 5vw;
+  margin-right: 5vw;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+.button {
   float: right;
   margin-top: 1vw;
+}
+.tiles {
+  margin-top:3vw ;
+  margin-left: 12vw;
+  margin-right: 12vw;
+}
+p {
+  font-size: 14px;
+  text-align: justify;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+.card{
+    margin-bottom: 1vw;
 }
 </style>
