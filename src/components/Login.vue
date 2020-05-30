4dd95c5c1abb69db5f12db0236ddacea9e4d3fb4
Subin Siby <mail@subinsb.com>
2020-05-30 20:32:41 +0530

    axios login & register

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,27 +1,41 @@
 <template>
-    <section>
-        <h1 class="title">Log in</h1>
-        <div class="loginform container">
-        <b-field label="Username" type="is-primary" class="info">
-            <b-input maxlength="30" placeholder="Enter your username"></b-input>
-        </b-field>
+  <section>
+    <h1 class="title">Log in</h1>
+    <form class="loginform container" @submit="submitForm">
+      <b-field label="Username" type="is-primary" class="info">
+        <b-input maxlength="30" placeholder="Enter your username"></b-input>
+      </b-field>
 
-        <b-field label="Password" type="is-primary" class="info">
-            <b-input type="password" placeholder="Enter your password"></b-input>
-        </b-field>
-        <b-field>
-                <button class="button is-primary">
-                    Login
-                </button>
-        </b-field>
-        </div>
-    </section>
+      <b-field label="Password" type="is-primary" class="info">
+        <b-input type="password" placeholder="Enter your password"></b-input>
+      </b-field>
+      <b-field>
+        <button class="button is-primary">
+          Login
+        </button>
+      </b-field>
+    </form>
+  </section>
 </template>
 
 
 <script>
 export default {
   name: 'Login',
+
+  methods: {
+    submitForm (e) {
+      e.preventDefault()
+
+      this.axios.post(this.$API.user.login, this.form).then((response) => {
+        if (response.status === 200) {
+          // TODO: store response.data.tokens
+        } else {
+          // TODO: show response.error
+        }
+      })
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,25 +2,38 @@ import Vue from 'vue'
 import App from './App.vue'
 import Navbar from './Navbar.vue'
 import Footer from './Footer.vue'
+
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import router from './router'
 
+Vue.use(VueAxios, axios)
 Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
+const API_URL = 'http://localhost:3000/api'
+Vue.prototype.$API = {
+  user: {
+    login: `${API_URL}/users/login`,
+    register: `${API_URL}/users/register`
+  }
+}
 
 new Vue({
   router,
-  render: h=>h(Navbar),
+  render: h => h(Navbar)
 }).$mount('#nav-bar')
 
 new Vue({
   router,
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
 
 new Vue({
-  render: h=>h(Footer),
+  render: h => h(Footer)
 }).$mount('#footer')
