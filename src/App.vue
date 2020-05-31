d5e60640671b7e3a6321b7920a2f2b422a78b23c
Kiran Johns <kiranjohns369@gmail.com>
2020-05-31 15:46:11 +0530

    roboto font

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,9 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;1,300&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
